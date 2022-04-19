c8e8ca4460cd7a9fba19ae634539f69df1d471fa
Nanogashi <103910661+Nanogashi@users.noreply.github.com>
2022-04-19 08:58:04 +0800

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,25 @@
 		
 
     ?>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Job Request</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Submit a request</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logout :D</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <table class="table table-bordered table-hover">
         <thead class="thead-dark">
             <tr>
