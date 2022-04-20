74bfb934103e805044f7c04f08945219339fa6a9
Nanogashi <103910661+Nanogashi@users.noreply.github.com>
2022-04-20 15:57:10 +0800

    Add files via upload

--- request_form.sql
+++ request_form.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2022 at 07:31 AM
+-- Generation Time: Apr 20, 2022 at 09:56 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'Monique Irish B. Llorca', 'moniqueirish.llorca@tup.edu.ph', '123456', 'admin'),
+(2, 'Ma. Angelica A. Lopez', 'maangelica.lopez@tup.edu.ph', 'e10adc3949ba59abbe56e057f20f883e', 'student'),
+(19, 'Juan Gabrielle Gomez', 'juangabrielle.gomez@tup.edu.ph', '12345', 'user');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `requests`
 --
 
@@ -38,24 +61,38 @@ CREATE TABLE `requests` (
   `software` varchar(255) NOT NULL,
   `dept` varchar(255) NOT NULL,
   `problem` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL DEFAULT 'pending'
+  `status` varchar(255) NOT NULL DEFAULT 'pending',
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `requests`
 --
 
-INSERT INTO `requests` (`id`, `request`, `dates`, `account_type`, `provided_email`, `provided_id`, `local_number`, `software`, `dept`, `problem`, `status`) VALUES
-(36, 'telephone repair', '', '', '', '', '789', '', 'CAFA', '', 'pending'),
-(37, 'daily time record', 'May 1 - 15, 2021', '', '', '', '', '', '', '', 'pending'),
-(38, 'ict repair equipment', '', '', '', '', '', '', 'CLA', 'Broken Computer', 'pending'),
-(39, 'others', '', '', '', '', '', '', 'IRTC', 'Need Cash', 'pending'),
-(40, 'reset password', '', 'TUP Web ERS', 'giann825@gmail.com', 'tupm-18-0229', '', '', '', '', 'pending'),
-(41, 'software installation', '', '', '', '', '', 'microsoft office', 'CIE', '', 'pending');
+INSERT INTO `requests` (`id`, `request`, `dates`, `account_type`, `provided_email`, `provided_id`, `local_number`, `software`, `dept`, `problem`, `status`, `email`) VALUES
+(36, 'telephone repair', '', '', '', '', '789', '', 'CAFA', '', 'pending', ''),
+(37, 'daily time record', 'May 1 - 15, 2021', '', '', '', '', '', '', '', 'pending', ''),
+(38, 'ict repair equipment', '', '', '', '', '', '', 'CLA', 'Broken Computer', 'pending', ''),
+(39, 'others', '', '', '', '', '', '', 'IRTC', 'Need Cash', 'pending', ''),
+(40, 'reset password', '', 'TUP Web ERS', 'giann825@gmail.com', 'tupm-18-0229', '', '', '', '', 'pending', ''),
+(41, 'software installation', '', '', '', '', '', 'microsoft office', 'CIE', '', 'pending', ''),
+(42, 'others', '', '', '', '', '', '', 'COS', 'Requesting new pc', 'pending', ''),
+(43, 'others', '', '', '', '', '', '', 'CLA', 'pending request for new PC', 'pending', ''),
+(44, 'ict repair equipment', '', '', '', '', '', '', 'CLA', 'adsfadf', 'pending', ''),
+(45, 'daily time record', 'May 1 - 15, 2021', '', '', '', '', '', '', '', 'pending', ''),
+(46, 'telephone repair', '', '', '', '', '111', '', 'COS', '', 'pending', 'moniqueirish.llorca@tup.edu.ph'),
+(47, 'reset password', '', 'TUP Portal', 'giann825@gmail.com', 'lkjlkjlkjlkjlkj', '', '', '', '', 'pending', 'juangabrielle.gomez@tup.edu.ph'),
+(48, 'biometric record', 'May 15 - 30, 2022', '', '', '', '', '', '', '', 'pending', 'moniqueirish.llorca@tup.edu.ph');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `requests`
@@ -68,10 +105,16 @@ ALTER TABLE `requests`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
