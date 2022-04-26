e607b4ebc6bcded245a9369d3ae0d28e72d978a8
Nanogashi <103910661+Nanogashi@users.noreply.github.com>
2022-04-26 15:37:37 +0800

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   ob_start();
-   session_start();
-   session_destroy();
-   session_unset($_SESSION['NAME']);
-   session_unset($_SESSION['ROLE']);
-   session_unset($_SESSION['ID']);
-   header("Location:index.php");
-   die();
